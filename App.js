c9002ab414096545ac92a7c3551c3f9d6230dac1
Abrorbek <abrorbekweb@gmail.com>
2025-04-18 11:29:56 +0500

    changing api key

--- App.js
+++ App.js
@@ -1,65 +1,64 @@
-import axios from 'axios';
-import * as Location from 'expo-location';
-import { useEffect, useState } from 'react';
-import { Alert, StyleSheet } from 'react-native';
-import Loader from './components/loader';
-import Weather from './components/weather';
+import axios from "axios";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
+import { Alert, StyleSheet } from "react-native";
+import Loader from "./components/loader";
+import Weather from "./components/weather";
 
-const API_KEY = '13988e2bb2d40da3e2804ef1ceec4225';
+const API_KEY = "960ed30431936501a9df534a1ffc2ed8";
 
 export default function App() {
-	const [isLoading, setIsLoading] = useState(true);
-	const [location, setLocation] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [location, setLocation] = useState(null);
 
-	const getWeather = async (latitude, longitude) => {
-		const { data } = await axios.get(
-			`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
-		);
-		setLocation(data);
-		setIsLoading(false);
-	};
+  const getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    setLocation(data);
+    setIsLoading(false);
+  };
 
-	const setWeather = async query => {
-		const { data } = await axios.get(
-			`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}&units=metric`
-		);
-		setLocation(data);
-		setIsLoading(false);
-	};
+  const setWeather = async (query) => {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}&units=metric`
+    );
+    setLocation(data);
+    setIsLoading(false);
+  };
 
-	const getLocation = async () => {
-		try {
-			const { status } =
-				await Location.requestForegroundPermissionsAsync();
-			if (status !== 'granted') {
-				Alert.alert('Permission to access location was denied');
-				return;
-			}
+  const getLocation = async () => {
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        Alert.alert("Permission to access location was denied");
+        return;
+      }
 
-			const {
-				coords: { latitude, longitude },
-			} = await Location.getCurrentPositionAsync({});
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync({});
 
-			getWeather(latitude, longitude);
-		} catch (error) {
-			Alert.alert("I can't find your current location, so bad ):");
-		}
-	};
+      getWeather(latitude, longitude);
+    } catch (error) {
+      Alert.alert("I can't find your current location, so bad ):");
+    }
+  };
 
-	useEffect(() => {
-		getLocation();
-	}, []);
+  useEffect(() => {
+    getLocation();
+  }, []);
 
-	return isLoading ? (
-		<Loader />
-	) : (
-		<Weather
-			setWeather={setWeather}
-			temp={location.main.temp}
-			name={location.name}
-			condition={location.weather[0].main}
-		/>
-	);
+  return isLoading ? (
+    <Loader />
+  ) : (
+    <Weather
+      setWeather={setWeather}
+      temp={location.main.temp}
+      name={location.name}
+      condition={location.weather[0].main}
+    />
+  );
 }
 
 const styles = StyleSheet.create({});
